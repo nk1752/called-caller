7e1f9ab1785e483cf5524b24eb323a65a07fc20d
nk1752 <71557302+nk1752@users.noreply.github.com>
2024-10-13 10:59:19 -0500

    functions

---
+++ .github/workflows/scripts/calculate_days.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Get the current date
+current_date=$(date)
+
+# Echo the current date
+echo "Current date and time: $current_date"
+
+# return the current date
+return $current_date
